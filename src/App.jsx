413f4e37b70791030d91adc883add35091803d33
Carecabeludo22 <rm99666@estudante.fieb.edu.br>
2026-03-10 11:41:42 -0300

    estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-// const - palavra reservada do javascript para criar constantes
-// App - nome do componente, deve comecar com letra maiuscula
-//() => sintaxe de função arrow (função anonima)
- 
- 
-const App = () => {
- 
-  // area do javascript do componente
-  //return - palavra reservada do javascript para retornar o conteudo do componente
- 
-  return (
-    <>{/** Fragmento estatico do componente */}
-      AULA REACTJS DSA2
-      {/*comentario em JSX*/}
-    </>
+import '.App.css';
+
+function App(){
+
+  return(
+    <div>
+      <h1>App</h1>
+      <p>Aula de DSA2</p>
+    </div>
   )
 }
-//export - palavra reservada do javascript para exportar o componente
-//default - palavra reservada do javascript para exportar o componete como padrao
-//App - nome do componente a ser exportado
+
 export default App;
- 
